deafabe8ac72f3d85bb49795777365d1101481ed
vaishukhamkar <158250153+vaishukhamkar@users.noreply.github.com>
2024-10-02 21:01:38 +0530

    Add files via upload

---
+++ upper_case.py
@@ -0,0 +1,19 @@
+lass MyClass:
+
+                def Get_String(self):
+
+                                self.MyStr=input("Enter any String: ")
+
+                def Print_String(self):
+
+                                s=self.MyStr
+
+                                print("String in Upper Case: " , s.upper())
+
+# main body
+
+Obj=MyClass()
+
+Obj.Get_String()
+
+Obj.Print_String()
